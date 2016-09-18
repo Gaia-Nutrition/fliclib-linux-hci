6acdd12b996fe27955821f141b9f149b55f56382
Benjamin Wulff <benjamin.wulff@gaia-nutrition.de>
2016-09-18 13:25:06 +0200

    Added first console commands to node client

--- clientlib/nodejs/example.js
+++ clientlib/nodejs/example.js
@@ -10,6 +10,7 @@ var FlicConnectionChannel = fliclib.FlicConnectionChannel;
 var FlicScanner = fliclib.FlicScanner;
 
 var client = new FlicClient("localhost", 5551);
+var displayState = 1;
 
 function listenToButton(bdAddr) {
 	var cc = new FlicConnectionChannel(bdAddr);
@@ -19,9 +20,27 @@ function listenToButton(bdAddr) {
 	});
 	cc.on("buttonSingleOrDoubleClick", function(clickType, wasQueued, timeDiff) {
 		if(clickType == "ButtonSingleClick"){
+			if(displayState == 1){
+				cmd.get(
+					'ls',
+					function(data){
+						console.log('would turn OFF the screen now!. And the current dir contains these files :\n\n',data);
+						displayState = 0;
+					}
+				);
+			}
+			else {
+				cmd.get(
+					'ls',
+					function(data){
+						console.log('would turn ON the screen now!. And the current dir contains these files :\n\n',data);
+						displayState = 1;
+					}
+				);
+			}
 			console.log('SingleClick Detected');
 		}
-		console.log(bdAddr + " " + clickType + " " + (wasQueued ? "wasQueued" : "notQueued") + " " + timeDiff + " seconds ago");
+		//console.log(bdAddr + " " + clickType + " " + (wasQueued ? "wasQueued" : "notQueued") + " " + timeDiff + " seconds ago");
 	});
 	cc.on("connectionStatusChanged", function(connectionStatus, disconnectReason) {
 		console.log(bdAddr + " " + connectionStatus + (connectionStatus == "Disconnected" ? " " + disconnectReason : ""));
